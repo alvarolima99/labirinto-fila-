d34e650012e50879d31d14115b8088b9eb717130
alvarolima99 <57735889+alvarolima99@users.noreply.github.com>
2022-05-26 16:32:04 -0300

    Add files via upload

---
+++ No.java
@@ -0,0 +1,42 @@
+public class No {
+
+    private int x, y;
+    private int dir;
+    public No(int i, int j)
+    {
+        this.x = i;
+        this.y = j;
+
+        this.dir = 0;
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+
+    public int getDir()
+    {
+        return dir;
+    }
+
+    public void setDir(int dir)
+    {
+        this.dir = dir;
+    }
+}
---
+++ Node.java
@@ -0,0 +1,17 @@
+public class Node<T> {
+    T data;
+    Node<T> previous;
+
+    public Node(T data, Node<T> previous) {
+        this.data = data;
+        this.previous = previous;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public Node<T> getPrevious() {
+        return previous;
+    }
+}
